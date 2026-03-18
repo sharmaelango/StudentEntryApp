a6369ac5c0c3cb04c916111df544bea109792788
Sharma <Sharma@SHARMA-DESKTOP>
2026-03-18 10:03:02 +0530

    Initial commit - Student Manager C# Project

---
+++ StudentManager/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace StudentManager
+{
+    public class Student
+    {
+        public int SNo { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string Class { get; set; } = string.Empty;
+        public string RegisterNumber { get; set; } = string.Empty;
+    }
+}
